259e327712352009f5dfe641a353f1fa2af4f1ef
Michael M. <trdMichaelM@yandex.ru>
2024-06-10 11:20:01 +0300

    GetSecurities and Entities are init

---
+++ CoinW.Entities/CoinW.Entities.cs
@@ -0,0 +1,64 @@
+﻿namespace CoinW.Entities;
+
+public enum ErrorCode
+{
+    Success = 200,
+    OperationFailed = 500,
+    NetworkError = 10001,
+    APINotFound = 10002,
+    ParameterError = 10003,
+    NoTradingPermission = 10004,
+    NoWithdrawPermission = 10005,
+    ApiKeyError = 10006,
+    SignatureError = 10007
+}
+
+public class MainResponse<T>
+{
+    public string code;
+    public T data;
+    public string failed;
+    public string msg;
+    public string success;
+}
+
+public class TradingPairInformation
+{
+    public string id;
+    public string last;
+    public string lowestAsk;
+    public string highestBid;
+    public string percentChange;
+    public string isFrozen;
+    public string high24hr;
+    public string low24hr;
+    public string baseVolume;
+}
+
+public class Coin
+{
+    public string chain;
+    public string maxQty;
+    public string minQty;
+    public string recharge;
+    public string symbol;
+    public string symbolId;
+    public string txFee;
+    public string withDraw;
+}
+
+public class TradingPair
+{
+    public string currencyPair;
+    public string currencyBase;
+    public string currencyQuote;
+    public string maxBuyCount;
+    public string minBuyCount;
+    public string pricePrecision;
+    public string countPrecision;
+    public string minBuyAmount;
+    public string maxBuyAmount;
+    public string minBuyPrice;
+    public string maxBuyPrice;
+    public string state;
+}
--- CoinW/Program.cs
+++ CoinW/Program.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using CoinW.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
-namespace CoinW;
+namespace CoinW.GetSecurities;
+
+public class Connector
+{
+    private readonly HttpClient httpClient = new HttpClient();
+
+    private const string httpUrl = "https://api.coinw.com";
+    private const string tradingPairUrl = "/api/v1/public?command=returnSymbol";
+    private const string coinInformationUrl = "/api/v1/public?command=returnCurrencies";
+    private const string tradingPairInformationUrl = "/api/v1/public?command=returnTicker";
+
+    public List<TradingPair> TradingPairs { get; private set; }
+    //public List<Coin> Coins { get; private set; }
+    public Dictionary<string, Coin> Coins { get; private set; }
+    //public List<TradingPairInformation> TradingPairsInformation { get; private set; }
+    public Dictionary<string, TradingPairInformation> TradingPairsInformation { get; private set; }
+
+    public Connector()
+    {
+        HttpResponseMessage httpResponseMessage = httpClient.GetAsync(httpUrl + tradingPairUrl).Result;
+        string response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        //MainResponse = JsonConvert.DeserializeAnonymousType<MainResponse>(Json, new MainResponse());
+        TradingPairs = JsonConvert.DeserializeObject<MainResponse<List<TradingPair>>>(response).data;
+
+        httpResponseMessage = httpClient.GetAsync(httpUrl + coinInformationUrl).Result;
+        response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        Coins = JsonConvert.DeserializeObject<MainResponse<Dictionary<string, Coin>>>(response).data;
+
+        httpResponseMessage = httpClient.GetAsync(httpUrl + tradingPairInformationUrl).Result;
+        response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        TradingPairsInformation = JsonConvert.DeserializeObject<MainResponse<Dictionary<string, TradingPairInformation>>>(response).data;
+    }
+}
 
 public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, CoinW API!");
+        Connector CoinW = new Connector();
+
+        Console.WriteLine(CoinW.TradingPairs);
+        Console.WriteLine(CoinW.Coins);
+        Console.WriteLine(CoinW.TradingPairsInformation);
+
+        Console.WriteLine("SUCCESS!!!");
     }
 }
